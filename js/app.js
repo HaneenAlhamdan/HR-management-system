5ad92a723511c60714d7a35169dc0abd5881d3d6
HaneenAlhamdan <henno_hamdan@yahoo.com>
2022-02-15 22:56:56 +0200

    add object

---
+++ js/app.js
@@ -0,0 +1,169 @@
+"Use strict";
+
+// function Salary()
+// {
+//     let maximm;
+//     let minimm;
+//      for( let i = 0 ;i <emploe ;i++){
+// if (this.Level == "Senior")
+// {
+//     maximm=2000;
+//     minimm=1500;
+// }
+// else if(this.Level == "Mid-Senior")
+// {
+//     maximm=1500;
+//     minimm=1000;   
+// }
+// else (this.Level == "Junior")
+// {
+//     maximm=1000;
+//     minimm=500;   
+// }
+// }
+var emploe = [ ] ;
+function emploees(Employee_ID , Full_Name , Department , Level, Image ,Salary )
+{
+    this.Employee_ID= Employee_ID;
+    this.Full_Name=Full_Name;
+    this.Department= Department;
+    this.Level= Level;
+    this.Image=Image;
+    this.Salary=0;
+    emploe.push(this);
+
+
+}
+
+emploees.prototype.getSalary = function()
+{
+    let maximm;
+    let minimm;
+
+    if (this.Level == "Senior")
+    {
+        maximm=2000;
+        minimm=1500;
+    }
+    else if(this.Level == "Mid-Senior")
+    {
+        maximm=1500;
+        minimm=1000;   
+    }
+    else (this.Level == "Junior")
+    {
+        maximm=1000;
+        minimm=500;   
+    }
+  let totalsal= gtrndinteger(minimm,maximm);
+  this.Salary = totalsal - totalsal * 0.075 ;
+
+}
+
+function gtrndinteger (minimm,maximm){
+    return Math.floor(Math.random() * (maximm- minimm +1)) + minimm ;
+}
+
+
+let Ghazi_Samer = new emploees (1000 ,"Ghazi Samer" , "Administration ", "Senior" ,"URL");
+let Lana_Ali = new emploees (1001 ,"Lana Ali" , "Finance ", "Senior" ,"URL");
+let Tamara_Ayoub = new emploees (1002 ,"Tamara Ayoub" , "Marketing ", "Senior" ,"URL");
+let Safi_Walid = new emploees (1002 ,"Safi Walid" , "Administration ", "Mid-Senior" ,"URL");
+let Omar_Zaid = new emploees (1004 ,"Omar Zaid" , "Development ", "Senior" ,"URL");
+let Rana_Saleh = new emploees (1005 ,"Rana Saleh" , "Development ", "Junior" ,"URL");
+let Hadi_Ahmad = new emploees (1006 ,"Hadi Ahmad" , "Finance ", "Mid-Senior" ,"URL");
+
+
+
+ 
+
+ console .log(emploe);
+ for(let x=0 ; x< emploe.length; x++)
+ {
+    emploees. prototype. render = function(){
+        document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.Full_Name}: ${this.Salary}</h4>`);
+        };
+(`<h5 style=`)
+     emploe[x].getSalary();
+     emploe[x].render();
+ };
+
+ //////////////////////////////////////////////////////////////////////////////
+ let employee_id  = function(start,range){
+    let getRandoom = Math.floor((Math.random() * range)+ start);
+    while( employee_id  > range){
+        getRandoom = Math.floor((Math.random() * range)+ start);
+    }
+    return getRandoom;
+
+};
+
+console.log(employee_id(1000,9000));
+
+////////////////////////////////////////////////////////////////////////////////
+
+// const form = document.querySelector('.form');
+//     submitInput= form[0].querySelector('input[type="submit"');
+// function getDataForm (h){
+//     h.preventDafault();
+//     var formData = new formData(form[0]);
+
+//     alert(formData.get('Full Name') + '-' + formData.get('Department') 
+//     + '-' + formData.get('Image link') + '-' + formData.get('Level') 
+//     + '-' + formData.get('Salary') );
+// }
+
+// document.addEventListener('loded' , function(){
+//    submitInput.addEventListener( 'click', getDataForm, false);
+//    } ,false);
+
+////////////////////////////////////////////////////////////////////////////////////////
+function clickhand(event){
+    event.preventDefault()
+
+    var Full_Name= event.target.full_Name.value;
+    
+   
+
+    var Department= event.target.department.value;
+   
+    var Image_link= event.target.imag.value;
+   
+    
+
+    var Level= event.target.level.value;
+
+ var Salary= event.target.getSalary().value;
+
+    let new_employee = new emploees (employee_id ,Full_Name ,Department, Level ,Image_link , getSalary());
+   
+
+    
+   
+   console.log(new_employee);
+}
+var form =document.getElementById('form')
+
+form.addEventListener('submit', clickhand)
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+var submit=document.getElementById("submit");
+submit.addEventListener("click", displayDatails);
+
+var card=1;
+
+function displayDatails(){
+    var Full_Name= document.getElementById("Full_Name").value;
+    var department= document.getElementById("department").value;
+    var imag= document.getElementById("imag").value;
+    var level= document.getElementById("level").value;
+    // var Salary= document.getElementById("getSalary()").value;
+    
+    document.getElementById('result').innerHTML=(`${Full_Name} + ${department} + ${imag} + ${level}`);
+
+   
+
+}
